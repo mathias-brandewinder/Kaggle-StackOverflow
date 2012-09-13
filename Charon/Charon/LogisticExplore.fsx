4f00070bd2af49e06ff3c483ec49cb2088f66658
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2012-09-13 03:33:42 -0700

    More logistic regression exploration
    ... big load of fail so far.

--- Charon/Charon/LogisticExplore.fsx
+++ Charon/Charon/LogisticExplore.fsx
@@ -6,6 +6,8 @@
 #load "LogisticRegression.fs"
 System.IO.Directory.SetCurrentDirectory(__SOURCE_DIRECTORY__)
 
+open System
+open System.Text
 open Charon.Data
 open Charon.Preprocessing
 open Charon.Distributions
@@ -41,18 +43,55 @@ let sample =
     
 let trainSet, validateSet = split sample trainPct
 
+//(float)post.Undeleted;
+let experience (post: Charon.Post) =
+    (post.PostDate - post.UserDate).Days
+
+let numbersFrom (post: Charon.Post) =
+    [ (float)post.Reputation; 
+      (float)post.Undeleted; 
+      (float)post.UserDate.Ticks; 
+      (float)(experience post); 
+      (float)post.Tags.Length; 
+      (float)post.Body.Length ] 
+
 let numeric (data: (Charon.Post * string) list) =
     data 
+    |> List.filter (fun (post, cat) ->
+        (stringToCategory cat) = TooLocal || (stringToCategory cat) = NotReal)
     |> List.map (fun (post, cat) ->
         let label = 
-            match (simplified cat) with
-            | "open" -> 1.0
-            | _      -> 0.0
-        label, [ 1.0; (float)post.Reputation ])
+            match (stringToCategory cat) with
+            | TooLocal -> 1.0
+            | _       -> 0.0
+        label, 1.0 :: (numbersFrom post))
 
 let nTraining = numeric trainSet
 let nValidate = numeric validateSet
-let w = train nTraining 1000 0.01
+let w = train nTraining 100 0.01
 let classifier = classify w
 
-let results = nValidate |> List.map (fun (lab, obs) -> lab, classifier obs) 
+let stats (predictions: (float * float) seq) =     
+    let correct =
+        predictions 
+        |> Seq.map (fun (real, pred) -> 
+            real,
+            if real = 0.0 && pred < 0.5 || real = 1.0 && pred > 0.5 
+            then 1.0 
+            else 0.0)
+    correct
+    |> Seq.averageBy snd
+    |> printfn "Overall correctly classified: %f"
+    correct
+    |> Seq.filter (fun (real, pred) -> real = 1.0)
+    |> Seq.averageBy snd
+    |> printfn "Group 1 correctly classified: %f"
+    correct
+    |> Seq.filter (fun (real, pred) -> real = 0.0)
+    |> Seq.averageBy snd
+    |> printfn "Group 0 correctly classified: %f"
+
+let results = 
+    nValidate 
+    |> List.map (fun (lab, obs) -> lab, classifier (List.tail obs))
+    |> stats
